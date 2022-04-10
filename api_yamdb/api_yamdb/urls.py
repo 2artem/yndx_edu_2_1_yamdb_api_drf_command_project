97e66c0cf090da3b10700e1d012e78c36c105af0
Артем <94477441+2artem@users.noreply.github.com>
2022-04-10 23:39:54 +0300

    Update urls.py

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -5,12 +5,11 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('api/', include('users.urls')),
+    path('api/', include('reviews.urls')),
     path('admin/', admin.site.urls),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
 ]
